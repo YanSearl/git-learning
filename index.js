8c41b49717d5591176f3a4a386f291166f316f4c
richardzae <richardzae@gmail.com>
2016-02-22 16:17:24 +0800

    stumansys

---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/2/22.
+ */
+var app  = require('./lib');
+console.log(app);
---
+++ lib.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/2/22.
+ */
+module.exports={
+   theme : "沈逗比是CSS大神"
+};
